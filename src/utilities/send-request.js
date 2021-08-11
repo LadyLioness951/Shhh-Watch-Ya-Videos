daf859c57115ef1c2286bdc2e234bd29e7683fee
Megan Hays <ladylioness@megans-imac.lan>
2021-08-11 09:10:48 -0700

    Add payload to send request however still getting same error from yesterday.

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -1,6 +1,6 @@
 import { getToken } from './users-service';
 
-export default async function sendRequest(url, method = 'GET', payload = null) {
+export default async function sendRequest(url, method = 'GET', payload = null, payloadIsFormData = false) {
   // Fetch takes an optional options object as the 2nd argument
   // used to include a data payload, set headers, etc. 
   const options = { method };
@@ -15,6 +15,11 @@ export default async function sendRequest(url, method = 'GET', payload = null) {
     // Add token to an Authorization header
     // Prefacing with 'Bearer' is recommended in the HTTP specification
     options.headers.Authorization = `Bearer ${token}`;
+    // If payload is a FormData object, fetch automatically
+    // sets the Content-Type to 'multipart/form-data'
+    options.headers = payloadIsFormData ? {} : { 'Content-Type': 'application/json' };
+    // If payload is a FormData object, don't stringify it
+    options.body = payloadIsFormData ? payload : JSON.stringify(payload);
   }
   const res = await fetch(url, options);
   // res.ok will be false if the status code set to 4xx in the controller action
